c48fdab1b6963f0516a7232605dc1a1f8e553b89
LESAGE Arno <80953754+LesageArno@users.noreply.github.com>
2025-11-24 20:59:24 +0100

    Delete regul directory

--- regul/get_photo.ino
+++
@@ -1,6 +0,0 @@
-#define photoPin 33
-
-// Luminosity getter
-int getPhoto() {
-  return analogRead(photoPin);//(4095-analogRead(photoPin))/4095;
-}
--- regul/update_weighted_luminosity.ino
+++
@@ -1,5 +0,0 @@
-// update weigthed luminosity using exponential decay, this avoid enabling alarm with 
-float updateWeightedLuminosity(int luminosity, float avgLightDecay, float weightedLuminosity) {
-  weightedLuminosity = avgLightDecay * weightedLuminosity + (1-avgLightDecay) * luminosity;
-  return weightedLuminosity;
-}
